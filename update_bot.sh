5e15a7d288427471b83950f72924a9012d8b69ca
Adiar1 <river96riley@gmail.com>
2024-12-03 22:09:32 -0500

    commit

---
+++ update_bot.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Find and kill the bot process
+echo "Stopping the bot..."
+pkill -f main.py
+
+# Pull the latest changes from Git
+echo "Pulling latest changes..."
+git pull origin master
+
+# Restart the bot
+echo "Restarting the bot..."
+source /root/Clash-Royale-Bot
+python3 main.py &
