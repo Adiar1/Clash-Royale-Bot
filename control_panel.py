e73d1380fce7a29dd141f2bde56e457ba795e2f3
Adiar1 <river96riley@gmail.com>
2024-12-24 10:43:50 -0500

    backdashed whotopromote.py and whotokick.py name display

--- control_panel.py
+++ control_panel.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, send_file, abort, session
 import os
 import subprocess
-import signal
 import sqlite3
 from dotenv import load_dotenv
 from functools import wraps
